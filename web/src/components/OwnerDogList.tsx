54a52c0887ee85894753afe558cf78a0ddd9cca7
Rafael Montaño Boraita <boraita@gmail.com>
2021-10-18 19:54:15 +0200

    Fix build into docker

--- web/src/components/OwnerDogList.tsx
+++ web/src/components/OwnerDogList.tsx
@@ -1,22 +1,23 @@
-import {Button} from '@material-ui/core';
-import {AxiosResponse} from 'axios';
-import React, {useState} from 'react';
-import api from '../core/service/api';
-import {ApiResources} from '../core/service/ApiResources';
-import {resolveApiPath} from '../core/service/resolvePath';
+import { Button } from '@material-ui/core';
+import { AxiosResponse } from 'axios';
+import api from 'core/service/api';
+import { ApiResources } from 'core/service/ApiResources';
+import { resolveApiPath } from 'core/service/resolvePath';
+import { owner } from 'models/owner';
+import React, { useState } from 'react';
 
 export const OwnerDogList = () => {
   const [owners, setOwners] = useState([]);
 
   const handleClick = () => {
     const path = resolveApiPath(ApiResources.OWNERS);
-    api.get(path).then((apiResponse: AxiosResponse) =>setOwners(apiResponse?.data));
+    api.get(path).then((apiResponse: AxiosResponse) => setOwners(apiResponse?.data));
   };
   return (
     <>
       <Button variant="contained" onClick={handleClick}>Get Owners</Button>
       {
-        owners.map((owner) => (<p key={owner.name}>{owner}</p>))
+        owners.map((owner: owner) => (<p key={owner.name}>{owner}</p>))
       }
     </>
   );
