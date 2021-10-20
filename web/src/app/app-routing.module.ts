351f7c930ebdb4f65d07386c7f382edba28b9173
Rafael Montaño Boraita <boraita@gmail.com>
2021-10-21 00:05:50 +0200

    Add schedule

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'schedule',
+    loadChildren: () => import('./components/schedule/schedule.module').then(m => m.ScheduleModule)
+  },
+  {
+    path: '',
+    redirectTo: 'schedule',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ web/src/app/components/schedule/schedule.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CalendarComponent } from './calendar/calendar.component';
+
+@NgModule({
+  declarations: [
+    CalendarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ScheduleModule { }
