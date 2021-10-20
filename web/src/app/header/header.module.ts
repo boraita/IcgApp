175850c6e4cfda33641cdf8e4699a3e0c6778445
Rafael Montaño Boraita <boraita@gmail.com>
2021-10-20 23:07:56 +0200

    Add create toolbar and menu

--- web/src/app/header/header.module.ts
+++ web/src/app/header/header.module.ts
@@ -2,18 +2,22 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatMenuModule
   ],
   exports: [HeaderComponent]
 })
