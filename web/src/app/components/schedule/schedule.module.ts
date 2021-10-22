20d39989c5aa1bbf37730af7b3b0466a342403f5
Rafael Montaño Boraita <boraita@gmail.com>
2021-10-23 00:41:50 +0200

    Add route component

--- web/src/app/components/schedule/schedule.module.ts
+++ web/src/app/components/schedule/schedule.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CalendarComponent } from './calendar/calendar.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
     CalendarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: CalendarComponent }] as Routes)
   ]
 })
 export class ScheduleModule { }
