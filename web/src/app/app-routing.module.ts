256f9cb73530abe76d4043f4fd499f1741141dff
Rafael Montaño Boraita <boraita@gmail.com>
2021-10-28 11:54:32 +0200

    Add modules and update menu links

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+  },
+  {
     path: 'schedule',
     loadChildren: () => import('./components/schedule/schedule.module').then(m => m.ScheduleModule)
   },
   {
-    path: '',
-    redirectTo: 'schedule',
-    pathMatch: 'full'
+    path: 'report',
+    loadChildren: () => import('./components/reports/reports.module').then(m => m.ReportsModule)
   }
 ];
 
