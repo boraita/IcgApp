96a03b3e573403c4cb05246d64e02913439afe10
Rafael Montaño Boraita <boraita@gmail.com>
2021-11-27 23:33:30 +0100

    Add user call backend

--- api/src/db/models/users/users.module.ts
+++ api/src/db/models/users/users.module.ts
@@ -7,5 +7,6 @@ import { UsersService } from './users.service';
 @Module({
 	imports: [TypeOrmModule.forFeature([Users])],
 	providers: [UsersService, UsersResolver],
+	exports: [UsersResolver],
 })
-export class UsersModule {}
+export class UsersDbModule {}
